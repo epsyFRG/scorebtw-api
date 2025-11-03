2e897421812e0e3d367dad0e9e79bacf729f5815
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-11-03 18:03:37 +0100

    update dto

---
+++ src/main/java/com/scorebtw/api/payload/UserDTO.java
@@ -0,0 +1,13 @@
+package com.scorebtw.api.payload;
+
+import java.util.Set;
+
+public record UserDTO(
+        Long id,
+        String username,
+        String email,
+        Set<String> roles
+) {
+}
+
+
